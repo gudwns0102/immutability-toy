c42464ae8e4bed619e2d184ceb809b6b2d3bee44
김형준 <brojun@gimhyeongjun-ui-MacBook-Pro.local>
2018-09-07 04:32:23 +0900

    Exclude __test__ from compiling

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,8 @@ import {
   Command,
   SplicePayload
 } from './index.d';
+import isCommand from './lib/isCommand';
+import trim from './lib/trim';
 
 const update = (state: any, command: any): object => {
   const commandKey = isCommand(command);
@@ -56,43 +58,4 @@ const update = (state: any, command: any): object => {
   );
 }
 
-export const isCommand = (command: any): Command | boolean => {
-  const commands: Command[] = ['$set', '$push', '$unshift', '$apply', '$merge', '$splice'];
-  if (command && command.constructor === Object) {
-    const keys: string[] = Object.keys(command);
-    const commandKey = keys.find((key: string) => commands.includes(key as any)) as Command | undefined;
-    if (commandKey && keys.length !== 1) {
-      throw new Error('Command has invalid form!');
-    } else if (commandKey && keys.length === 1) {
-      const payload = trim(command);
-      const typeChecker: { [key in Command]: ($payload: any) => boolean } = {
-        $set: ($payload: any) => true,
-        $push: ($payload: any) => $payload && $payload.constructor === Array,
-        $unshift: ($payload: any) => $payload && $payload.constructor === Array,
-        $apply: ($payload: any) => $payload && $payload.constructor === Function,
-        $merge: ($payload: any) => $payload && $payload.constructor === Object,
-        $splice: ($payload: any) =>
-          $payload &&
-          $payload.constructor === Array &&
-          $payload[0].constructor === Array &&
-          typeof $payload[0][0] === 'number',
-      }
-
-      const typeValid = typeChecker[commandKey](payload);
-
-      if (!typeValid) {
-        throw new TypeError('Command has invalid payload');
-      }
-
-      return commandKey;
-    }
-  }
-
-  return false;
-}
-
-export const trim = (command: any) => {
-  return command[Object.keys(command)[0]];
-}
-
-export default update;
+export = update;
